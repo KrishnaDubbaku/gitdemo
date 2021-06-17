37c30fb1b43db1e8250d2f263e4d11199b1cfaa2
KrishnaDubbaku <venkatvv212@gmail.com>
2021-06-17 12:22:04 +0530

    first commit

---
+++ src/main/java/boot/git/controller/StudentController.java
@@ -0,0 +1,15 @@
+package boot.git.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController
+{
+@GetMapping("/show")
+public String fecthStudent()
+{
+return "The Student name is Krishna";	
+}
+
+}
